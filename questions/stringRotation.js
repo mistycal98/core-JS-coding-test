1eb80f85731b918260f7ba6eb86cfb42c20a83b7
Tushar Mistry <tusharmistry@MacBook-Air.local>
2020-12-04 17:07:15 +0530

    Done and Dusted

--- questions/stringRotation.js
+++ questions/stringRotation.js
@@ -4,7 +4,30 @@ const rotateString = (str, n, direction) => {
   //
   // input str = random ,1, right
   // output mrando
-
-};
-
+  let result = '';
+  const leftRotate = () => {
+    strFirst = str.slice(0, n);
+    strSecond = str.slice(n);
+    strComplete = strSecond + strFirst;
+    return strComplete;
+  };
+  const rightRotate = () => {
+    strFirst = str.slice(-n);
+    strSecond = str.slice(0, (str.length) - n);
+    strComplete = strFirst + strSecond;
+    return strComplete;
+  };
+  if (str || n || direction) {
+    if (direction === 'right') {
+      result = rightRotate();
+    } else if (direction === 'left') {
+      result = leftRotate();
+    } else {
+      throw Error('Invalid Direction');
+    }
+  } else {
+    throw Error('Invalid Input');
+  }
+  return result;
+}
 module.exports = rotateString;
